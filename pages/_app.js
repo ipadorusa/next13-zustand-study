27c2d182b4f854b98a68e288aa6dbf92800b387b
ipadorusa <ipadorusa@gmail.com>
2023-03-16 16:56:43 +0900

    change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from '../components/layout'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+const queryClient = new QueryClient()
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </Layout>
   )
 }
