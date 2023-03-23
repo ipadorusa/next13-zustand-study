bd76f7d72609b9e1b466427c267d2e2e716f96f0
ipadorusa <ipadorusa@gmail.com>
2023-03-23 12:22:11 +0900

    change

--- src/components/organism/header/index.jsx
+++ src/components/organism/header/index.jsx
@@ -5,10 +5,10 @@ export default function Header() {
   return (
     <header id="sri_header" className={style.header}>
       <div className={style.area_logo}>
-        <Link href="" className={style.link_saramin}>
+        <Link href="/about" className={style.link_saramin}>
           <span className="blind">사람인 메인으로 가기</span>
         </Link>
-        <Link href="" title="사람인 기업서비스 MUST" className={style.link_logo}>
+        <Link href="/blog" title="사람인 기업서비스 MUST" className={style.link_logo}>
           <Ci />
         </Link>
       </div>
